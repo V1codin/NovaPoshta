0d32b48a0bfa52c30a1fa4cac2b676a080bc4e50
V1codin <blindmonk46@gmail.com>
2020-10-30 11:51:13 +0200

    n

--- src/system/Reducer/Reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import warning from "../../components/modules/Warning/components/setts/reducer";
-import inputData from "../../components/blocks/InputContainer/reducer";
-import history from "../../components/blocks/HistoryCointainer/redux/reducer";
-
-import cities from "../CitiesReducer/reducer";
-
-export default combineReducers({ warning, inputData, history, cities });
